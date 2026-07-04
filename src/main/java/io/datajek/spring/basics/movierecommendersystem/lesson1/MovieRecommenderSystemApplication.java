a4f9ec9607e8303cca3cb49171b95013fe33a2bb
rachidout <rachidoutsila7@gmail.com>
2026-07-04 03:16:13 +0100

    initial commit

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/MovieRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/RecommenderImplementation.java
@@ -0,0 +1,8 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson1;
+
+public class RecommenderImplementation {
+
+    public String[] recommendMovies(String movie) {
+        return new String[] {"M1","M2","M3"};
+    }
+}
